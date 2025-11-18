c76772c9f0b0603edb583337980da406a607e2ff
Jy <ym31215@gmail.com>
2025-11-18 20:49:18 +0800

    component: 自定義 input

--- components/ETextField.vue
+++ components/ETextField.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
-    <label></label>
-    <input />
+  <div >
+    <label :for="elementId"  class="text-white font-bold block mb-1 text-base">{{ props.label }}</label>
+    <input :id="elementId" :type="props.type" class="w-full box-border bg-transparent border border-solid border-gray-400 rounded text-base text-white font-bold px-2 py-1" :value="props.modelValue" @input="handleInput"/>
   </div>
 </template>
 
@@ -9,9 +9,48 @@
 interface Props {
   id?: string // 若使用者有輸入，以使用者輸入的為主，若沒有請產出一個唯一 ID
   label?: string
+  type?: string
+  modelValue?: string
 }
 
-const props = withDefaults(defineProps<Props>(), {})
+const props = withDefaults(defineProps<Props>(), {
+  id: '',
+  label: '',
+  type: 'text',
+  modelValue:'',
+})
+
+const generatedId = ref('')
+
+onMounted(() => {
+  generatedId.value = crypto.randomUUID()
+})
+
+const elementId = computed(() => {
+    return props.id || `field-${generatedId.value}`
+})
+
+const emit = defineEmits<{
+  (e: 'update:modelValue', value: string): void
+}>()
+
+const handleInput= (event: Event)=>{
+  const target = event.target as HTMLInputElement
+  emit('update:modelValue', target.value)
+}
+
+
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+// 隱藏 number input 的上下箭頭
+input[type='number'] {
+  -moz-appearance: textfield;
+
+  &::-webkit-outer-spin-button,
+  &::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+}
+</style>
