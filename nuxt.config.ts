03f321492867251756a4cdf503de4f78d92a28de
Jy <ym31215@gmail.com>
2025-11-19 17:37:02 +0800

    chore: github-page deploy setting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,15 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
 
+  nitro: {
+    baseURL: '/test_frontend_v2/', 
+  },
+
+  app: {
+    baseURL: '/test_frontend_v2/',
+    buildAssetsDir: '/_nuxt/'
+  },
+
   modules: ['@nuxtjs/i18n', '@pinia/nuxt', '@nuxt/image', '@unocss/nuxt', '@nuxtjs/google-fonts'],
 
   googleFonts: {
